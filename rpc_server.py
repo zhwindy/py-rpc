3ddd76597b89b0de2c96ebd3c3f93eec92e8f25d
zhaopengfei <zpf@zhaopengfeideMacBook-Pro.local>
2019-05-07 19:55:26 +0800

    rpc base

---
+++ rpc_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding=utf-8
+import socket
+
+HOST, PORT = SERVER_ADDRESS = ('localhost', 8888)
+
+
+def rpc_server():
+    """
+    rpc 服务端
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(SERVER_ADDRESS)
+    sock.listen()
+    print(f"Socket server start at {HOST} {PORT}")
+
+    while True:
+        client_conn, client_address = sock.accept()
+        request_data = client_conn.recv(1024)
+        print(client_address, request_data)
+        response = b"OK Request is success received!"
+        client_conn.send(response)
+        client_conn.close()
+
+
+if __name__ == "__main__":
+    rpc_server()
