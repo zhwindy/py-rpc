5102da82db77e3c9b03065d251d911a61179fb2a
zhaopengfei <zpf@zhaopengfeideMacBook-Pro.local>
2019-05-24 20:10:22 +0800

    fix some

---
+++ application/__init__.py
@@ -0,0 +1,7 @@
+# RPC服务器server:
+# 1. preforking多进程异步服务器
+# 2. 使用zookeeper实现分布式
+
+# RPC客户端client:
+# 1. 服务发现
+# 2. 可用服务器列表维护
--- application/server.py
+++ application/server.py
@@ -235,7 +235,6 @@ if __name__ == "__main__":
     else:
         host = sys.argv[1]
         port = int(sys.argv[2])
-    print(host, port)
     RPCServer(host, port)
     print("RPC SERVER starting...")
     asyncore.loop()
