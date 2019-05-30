e2235f11a64610062aee3a70d82c4af3191f3431
zhaopengfei <zpf@zhaopengfeideMacBook-Pro.local>
2019-05-30 10:52:56 +0800

    server classify

---
+++ util/varint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding=utf-8
+import time
+
+
+def main():
+    """
+    整型数据编码: int8,int32,int64.unint,long
+    采用变长字节数
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
